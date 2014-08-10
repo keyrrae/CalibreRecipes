4ac8dc7ed785d986584c3070de731d530b516b25
keyrrae <zjuwangx@gmail.com>
2014-08-10 17:00:07 +0800

    Update XuansCollection.py

--- XuansCollection.py
+++ XuansCollection.py
@@ -34,7 +34,25 @@ class XuansCollection(BasicNewsRecipe):
                  h3 { text-align: center; font-size: 125%; font-weight: bold; } \
                  h4, h5, h6 { text-align: center; font-size: 100%; font-weight: bold; }'
 
-    feeds          = [(u'The Smart Set', u'http://thesmartset.com/rss/default.xml'), (u'The New York Review of Books', u'http://feeds.feedburner.com/nybooks'), (u'B&N review', u'http://bnreview.barnesandnoble.com/bnreview/plugins/custom/barnesandnoble/bnreview/rss_full'),(u'LA Review of Books', u'http://lareviewofbooks.org/feed/?ver=2'), (u'Public Books', u'http://www.publicbooks.org/rss'),(u'The Scientist',u'http://www.the-scientist.com/?rss.feed/'),(u"Harper's Magazine",u'http://harpers.org/feed/'),(u'NME - Music Review',u'http://feeds2.feedburner.com/nme/SdML'),(u'The Paris Review',u'http://feeds.feedburner.com/TheParisReviewBlog'),(u'City Journal', u'http://www.city-journal.org/cj.xml'),(u'Open Letters - an Art and Literature Review',u'http://www.openlettersmonthly.com/feed/'),(u'Pacific Standard',u'http://www.psmag.com/feed/atom/'),(u'The New Criterion',u'http://www.newcriterion.com/xml/lastissue.cfm'),(u'The Rational Association',u'http://feeds.feedburner.com/RaBlogFull'),(u'Edge',u'http://edge.org/feed'),(u'Stand Point',u'http://feeds.feedburner.com/WelcomeToStandpoint'),(u'n+1',u'https://nplusonemag.com/feed/')]
+    feeds= [(u'The Smart Set', u'http://thesmartset.com/rss/default.xml'), 
+(u'The New York Review of Books', u'http://feeds.feedburner.com/nybooks'), 
+(u'B&N review', u'http://bnreview.barnesandnoble.com/bnreview/plugins/custom/barnesandnoble/bnreview/rss_full'),
+(u'LA Review of Books', u'http://lareviewofbooks.org/feed/?ver=2'),
+ (u'Public Books', u'http://www.publicbooks.org/rss'),
+(u'The Scientist',u'http://www.the-scientist.com/?rss.feed/'),
+(u"Harper's Magazine",u'http://harpers.org/feed/'),
+(u'NME - Music Review',u'http://feeds2.feedburner.com/nme/SdML'),
+(u'The Paris Review',u'http://feeds.feedburner.com/TheParisReviewBlog'),
+(u'City Journal', u'http://www.city-journal.org/cj.xml'),
+(u'Open Letters - an Art and Literature Review',u'http://www.openlettersmonthly.com/feed/'),
+(u'Pacific Standard',u'http://www.psmag.com/feed/atom/'),
+(u'The New Criterion',u'http://www.newcriterion.com/xml/lastissue.cfm'),
+(u'The Rational Association',u'http://feeds.feedburner.com/RaBlogFull'),
+(u'Edge',u'http://edge.org/feed'),
+(u'Stand Point',u'http://feeds.feedburner.com/WelcomeToStandpoint'),
+(u'n+1',u'https://nplusonemag.com/feed/'),
+(u'Book Forum',u'http://bookforum.com/rss.xml'),
+(u'More Intelligent Life',u'http://moreintelligentlife.com/rss.xml')]
 
     def get_cover_url(self):
         #cover_url = 'http://www.aldaily.com/wp-content/themes/aldaily/images/header.gif'
